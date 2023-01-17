391c7812dbd52b6678609bfe1f26ac26f96f6910
Brian4755 <brian.mccune1@yahoo.com>
2023-01-17 10:26:48 -0600

    more functionality to handleclick event for addtodo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { useState, useRef } from "react";
 import TodoList from "./TodoList";
+import { v4 as uuidv4} from 'uuid'
 
 function App() {
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
 
   function handleAddTodo(e) {
-  
+    const name = todoNameRef.current.value 
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
   }
 
   return (
