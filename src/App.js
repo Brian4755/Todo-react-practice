1c225727e8d6d700626bc8f706f4c4867e08f98d
Brian4755 <brian.mccune1@yahoo.com>
2023-01-17 09:25:13 -0600

    button and input field display

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-
+import React from "react";
+import TodoList from "./TodoList";
 
 function App() {
-  return null
+  // const [todo, setTodo] = useState()
 
+
+  return (
+    <>
+      <TodoList />
+      <input type='text' />
+      <button>Add Todo</button>
+      <button>Clear Todo</button>
+    </>
+  )
 }
 
 export default App;
