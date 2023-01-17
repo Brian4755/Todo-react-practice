1327b62e421becf7ec18a18368d17ca91c3c97a6
Brian4755 <brian.mccune1@yahoo.com>
2023-01-17 10:00:45 -0600

    onclick event for add todo and useRef for todo input

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import TodoList from "./TodoList";
 
 function App() {
-  const [todos, setTodos] = useState({})
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
 
+  function handleAddTodo(e) {
+  
+  }
 
   return (
     <>
       <TodoList todos={todos}/>
-      <input type='text' />
-      <button>Add Todo</button>
+      <input ref={ todoNameRef } type='text' />
+      <button onClick={ handleAddTodo }>Add Todo</button>
       <button>Clear Todo</button>
       <div>0 left to do</div>
     </>
